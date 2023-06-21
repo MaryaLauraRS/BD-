0f95f7a72055cd9c6276894109fd4cf6dd2631fb
MaryaLauraRS <marya.santos@estudante.ifms.edu.br>
2023-06-20 22:12:50 -0400

    atualizado

--- score.js
+++ score.js
@@ -7,18 +7,18 @@ const router = express.Router();
 router.post("/", async (req, res) => {
     let col = await db.collection('score');
     let resul = col.insertOne(req.body);
-    res.send(out).status(204);
+    res.send(resul).status(204);
     // seu código aqui incert
 });
 
 
 // Pegar os 10 melhores resultados
 router.get("/", async (req, res) => {
-    let col = await db.historico.find('score');
-    let resul = await col.historico.find().sort({
+    let col = await db.collection('score');
+    let resul = await col.find().sort({
         pontos: -1
         }).limit(10).toArray();
-        res.send(out).status(200);
+        res.send(resul).status(200);
     // seu código aqui fait
 });
 
